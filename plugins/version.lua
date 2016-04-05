f047ae130d66f9f7847e041e64bc869e6ffa119e
mrlife <sina.e.1380@gmail.com>
2016-04-05 14:16:46 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v2 \n by BeatBot Team :) <3 \n edite and prsian translation by @mrlife"
+    return "Self-Bot v2 \n by BeatBot Team :) <3 \n edite and persian translation by @mrlife"
 end 
 return {
   patterns = {
